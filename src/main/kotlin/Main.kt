fef76cfc99cf0ddf1ac5ec55cdd23e8b7128a1f2
tukasingura <tukasingurarichard551@gmail.com>
2023-07-18 18:45:04 +0300

    hthe second commintt

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+    println(9)
+
+    //the print function brings the output but continues in the same line
+    //the println function breaks to the new line after running the code
+
+    //below is the code to the githuh
+    //
+
+
+    print("hello world")
+}
